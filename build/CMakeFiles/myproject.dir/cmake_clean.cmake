3242900c415134ea72f974abc52c0bb344320682
gcwhitfield <gwhitfieldjr@gmail.com>
2022-01-14 22:15:25 -0500

    Import boilerplate project

---
+++ build/CMakeFiles/myproject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myproject.dir/libs/glad/src/glad.c.o"
+  "CMakeFiles/myproject.dir/src/Mode.cpp.o"
+  "CMakeFiles/myproject.dir/src/main.cpp.o"
+  "myproject"
+  "myproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
